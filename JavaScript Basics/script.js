1368c4a685a712be117eeef42564560f65ddb24e
HibaBenAmmar <86246905+HibaBenAmmar@users.noreply.github.com>
2021-07-05 18:46:09 +0200

    Add files via upload

---
+++ JavaScript Basics/script.js
@@ -0,0 +1,31 @@
+console.log ('Hello Winc Academy');
+
+let Name='Hiba';
+console.log (Name);
+
+let x=4+4;
+console.log (x);
+let x="4" +"4";
+console.log (x);
+
+let x= 8+2;
+console.log (x);
+let x=8-2;
+console.log (x);
+let x=8*2;
+console.log (x);
+let x=8/2;
+console.log (x);
+let x=8%2;
+console.log (x);
+
+typeof {age:zevenendertig}
+console.log (age)
+
+
+
+
+
+
+
+
